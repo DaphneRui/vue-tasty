99158c2f3964ad28cbeed9913a273167a4e115f2
zhaoyifanlalala <853792408@qq.com>
2020-10-27 20:10:26 +0800

    request

--- src/components/errorModal/errorModal.vue
+++ src/components/errorModal/errorModal.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="body">
     <!-- <div class="organization-item new" @click="openModal"></div> -->
-    <button @click="openModal">点击</button>
+    <button @click="openModal">
+      点击
+    </button>
     <!-- <as-modal/> -->
-      
   </div>
 </template>
 
@@ -13,6 +14,6 @@ import { mapState } from 'vuex';
 import './errorModal.scss';
 
 export default {
-  
+
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -2,17 +2,11 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-/* vue-js-modal */
-import VModal from 'vue-js-modal'
+// /* vue-js-modal */
+// import VModal from 'vue-js-modal';
 
 Vue.config.productionTip = false;
 
-<<<<<<< HEAD
-Vue.use(VModal,{
-   dialog: true,
-   dynamic: true
-})
-=======
 /* 引入 vue-i18n */
 import VueI18n from 'vue-i18n';
 Vue.use(VueI18n);
@@ -24,7 +18,6 @@ const i18n = new VueI18n({
       'en': require('./common/i18n/en-US.json')
    }
 });
->>>>>>> 58cf15fd0213956e2b786617bdb0f2cc9f9247af
 
 new Vue({
    router,
