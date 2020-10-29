eece53ba9dcc50792640c290340a739c3d22c910
ruixiang <1900956090@qq.com>
2020-10-29 16:21:00 +0800

    restaurant渲染(未排序)

--- src/App.vue
+++ src/App.vue
@@ -38,17 +38,19 @@ export default {
    margin : 0;
 }
 .App{
-//   width : 1440px;
+  width : 1440px;
   padding : 0 120px;
   margin : auto;
-background-color : #f7f7f7;
+// background-color : #f7f7f7;
 }
 .main-content{
-  display : flex;
-  align-items : center;
-  justify-content : center;
-  width : 100%;
-  height : calc(100vh - 45px);
+  // display : flex;
+  // align-items : center;
+  // justify-content : center;
+  // width : 100%;
+  // height : calc(100vh - 45px);
+  position : relative;
+  top : 100px;
 }
 .test{
   border : 1px solid #000;
